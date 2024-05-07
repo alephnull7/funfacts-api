229f16334087d89f904c3f51b7555ece84a54251
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-05-07 12:43:04 -0700

    Add exception handler to all attempted requests

--- routes/statesRouter.js
+++ routes/statesRouter.js
@@ -3,34 +3,43 @@ const router = express.Router();
 const statesController = require('../controllers/statesController');
 const verifyStates = require('../middleware/verifyStates');
 
+async function attemptRequest(req, res, method) {
+    try {
+        await statesController[method](req, res);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Internal Server Error'});
+    }
+}
+
 router.route('/')
-    .get((req, res) => statesController.getStates(req, res));
+    .get((req, res) => attemptRequest(req, res, 'getStates'));
 
 router.route('/:state')
     .all(verifyStates)
-    .get((req, res) => statesController.getState(req, res));
+    .get((req, res) => attemptRequest(req, res, 'getState'));
 
 router.route('/:state/funfact')
     .all(verifyStates)
-    .get((req, res) => statesController.getFunFact(req, res))
-    .post((req, res) => statesController.createFunFact(req, res))
-    .patch((req, res) => statesController.updateFunFact(req, res))
-    .delete((req, res) => statesController.deleteFunFact(req, res));
+    .get((req, res) => attemptRequest(req, res, 'getFunFact'))
+    .post((req, res) => attemptRequest(req, res, 'createFunFact'))
+    .patch((req, res) => attemptRequest(req, res, 'updateFunFact'))
+    .delete((req, res) => attemptRequest(req, res, 'deleteFunFact'));
 
 router.route('/:state/capital')
     .all(verifyStates)
-    .get((req, res) => statesController.getStateCapital(req, res));
+    .get((req, res) => attemptRequest(req, res, 'getStateCapital'));
 
 router.route('/:state/nickname')
     .all(verifyStates)
-    .get((req, res) => statesController.getStateNickname(req, res));
+    .get((req, res) => attemptRequest(req, res, 'getStateNickname'));
 
 router.route('/:state/population')
     .all(verifyStates)
-    .get((req, res) => statesController.getStatePopulation(req, res));
+    .get((req, res) => attemptRequest(req, res, 'getStatePopulation'));
 
 router.route('/:state/admission')
     .all(verifyStates)
-    .get((req, res) => statesController.getStateAdmission(req, res));
+    .get((req, res) => attemptRequest(req, res, 'getStateAdmission'));
 
 module.exports = router;
